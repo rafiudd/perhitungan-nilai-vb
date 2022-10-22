cc34ef6a704adb16e6f970d07973b657b8355483
rafiudd <sirin6867@gmail.com>
2022-10-22 16:21:30 +0700

    Init Repository

---
+++ Test/Form1.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Presensi = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.input_presensi = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.input_responsi = New System.Windows.Forms.TextBox()
+        Me.input_tugas = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.input_uas = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.hasil_akhir = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Presensi
+        '
+        Me.Presensi.AutoSize = True
+        Me.Presensi.Location = New System.Drawing.Point(30, 35)
+        Me.Presensi.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Presensi.Name = "Presensi"
+        Me.Presensi.Size = New System.Drawing.Size(62, 20)
+        Me.Presensi.TabIndex = 0
+        Me.Presensi.Text = "Presensi"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(30, 338)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(90, 27)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Cek Hasil Akhir"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'input_presensi
+        '
+        Me.input_presensi.Location = New System.Drawing.Point(35, 60)
+        Me.input_presensi.Margin = New System.Windows.Forms.Padding(2)
+        Me.input_presensi.Name = "input_presensi"
+        Me.input_presensi.PlaceholderText = "Masukan Nilai Presensi"
+        Me.input_presensi.Size = New System.Drawing.Size(196, 27)
+        Me.input_presensi.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(30, 99)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Responsi"
+        '
+        'input_responsi
+        '
+        Me.input_responsi.Location = New System.Drawing.Point(32, 125)
+        Me.input_responsi.Margin = New System.Windows.Forms.Padding(2)
+        Me.input_responsi.Name = "input_responsi"
+        Me.input_responsi.PlaceholderText = "Masukan Nilai Responsi"
+        Me.input_responsi.Size = New System.Drawing.Size(196, 27)
+        Me.input_responsi.TabIndex = 5
+        '
+        'input_tugas
+        '
+        Me.input_tugas.Location = New System.Drawing.Point(32, 193)
+        Me.input_tugas.Margin = New System.Windows.Forms.Padding(2)
+        Me.input_tugas.Name = "input_tugas"
+        Me.input_tugas.PlaceholderText = "Masukan Nilai Tugas"
+        Me.input_tugas.Size = New System.Drawing.Size(196, 27)
+        Me.input_tugas.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(30, 168)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Nilai Tugas"
+        '
+        'input_uas
+        '
+        Me.input_uas.Location = New System.Drawing.Point(33, 259)
+        Me.input_uas.Margin = New System.Windows.Forms.Padding(2)
+        Me.input_uas.Name = "input_uas"
+        Me.input_uas.PlaceholderText = "Masukan Nilai UAS"
+        Me.input_uas.Size = New System.Drawing.Size(196, 27)
+        Me.input_uas.TabIndex = 9
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(30, 235)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 20)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Nilai UAS"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(299, 235)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(191, 20)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Hasil Akhir Nilai Mahasiswa"
+        '
+        'hasil_akhir
+        '
+        Me.hasil_akhir.AutoSize = True
+        Me.hasil_akhir.Font = New System.Drawing.Font("Segoe UI", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.hasil_akhir.Location = New System.Drawing.Point(299, 269)
+        Me.hasil_akhir.Name = "hasil_akhir"
+        Me.hasil_akhir.Size = New System.Drawing.Size(0, 37)
+        Me.hasil_akhir.TabIndex = 11
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(299, 40)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(115, 180)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Ketentuan Nilai " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "> 86      =  A" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "76 - 85  =  B" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "66 - 75  =  C" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "55 - 65  =  D" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "< " &
+    "56      =  E" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(30, 411)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(158, 40)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Rayhan Rafiud Darojat" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "STI202102221"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(601, 469)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.hasil_akhir)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.input_uas)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.input_tugas)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.input_responsi)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.input_presensi)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Presensi)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Presensi As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents input_presensi As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents input_responsi As TextBox
+    Friend WithEvents input_tugas As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents input_uas As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents hasil_akhir As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Test/Form1.vb
@@ -0,0 +1,80 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'Handle jika user tidak menginput form
+        If input_presensi.Text.Length = 0 Then
+            input_presensi.Text = 0
+        End If
+
+        If input_responsi.Text.Length = 0 Then
+            input_responsi.Text = 0
+        End If
+
+        If input_tugas.Text.Length = 0 Then
+            input_tugas.Text = 0
+        End If
+
+        If input_uas.Text.Length = 0 Then
+            input_uas.Text = 0
+        End If
+        'Handle jika user tidak menginput form
+
+        'Variable untuk menyimpan nilai huruf(A,B,C,D,E)
+        Dim nilai_huruf
+
+        'Jumlah semua hasil input
+        hasil_akhir.Text = Convert.ToInt64(input_presensi.Text) + Convert.ToInt64(input_responsi.Text) + Convert.ToInt64(input_tugas.Text) + Convert.ToInt64(input_uas.Text)
+        hasil_akhir.Text = Convert.ToInt64(hasil_akhir.Text)
+
+        If hasil_akhir.Text > 86 Then
+            nilai_huruf = " A"
+        ElseIf hasil_akhir.Text < 86 And hasil_akhir.Text > 76 Then
+            nilai_huruf = " B"
+        ElseIf hasil_akhir.Text < 76 And hasil_akhir.Text > 66 Then
+            nilai_huruf = " C"
+        ElseIf hasil_akhir.Text < 66 And hasil_akhir.Text > 56 Then
+            nilai_huruf = " D"
+        Else
+            nilai_huruf = " E"
+        End If
+
+        'Tambahkan nilai huruf ke hasil akhir
+        hasil_akhir.Text += nilai_huruf
+    End Sub
+
+    Private Sub input_presensi_TextChanged(sender As Object, e As EventArgs) Handles input_presensi.TextChanged
+        'Handle jika user menginput presensi lebih dari 20
+        If input_presensi.TextLength > 0 Then
+            If Convert.ToInt64(input_presensi.Text) > 20 Then
+                input_presensi.Text = "20"
+            End If
+        End If
+    End Sub
+
+    Private Sub input_responsi_TextChanged(sender As Object, e As EventArgs) Handles input_responsi.TextChanged
+        'Handle jika user menginput responsi lebih dari 10
+        If input_responsi.TextLength > 0 Then
+            If Convert.ToInt64(input_responsi.Text) > 10 Then
+                input_responsi.Text = "10"
+            End If
+        End If
+    End Sub
+
+    Private Sub input_tugas_TextChanged(sender As Object, e As EventArgs) Handles input_tugas.TextChanged
+        'Handle jika user menginput tugas lebih dari 25
+        If input_tugas.TextLength > 0 Then
+            If Convert.ToInt64(input_tugas.Text) > 25 Then
+                input_tugas.Text = "25"
+            End If
+        End If
+    End Sub
+
+    Private Sub input_uas_TextChanged(sender As Object, e As EventArgs) Handles input_uas.TextChanged
+        'Handle jika user menginput uas lebih dari 45
+        If input_uas.TextLength > 0 Then
+            If Convert.ToInt64(input_uas.Text) > 45 Then
+                input_uas.Text = "45"
+            End If
+        End If
+    End Sub
+End Class
